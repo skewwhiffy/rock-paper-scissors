d2d7096eed5c292f4cec6d0cdd9b2b24f26439ab
Kenny Hung <khung@instructors.2u.com>
2021-12-18 13:21:17 +0000

    Implementation

--- index.js
+++ index.js
@@ -2,7 +2,53 @@
 // confirm()
 // const value = prompt()
 // console.log('You put in ' + value)
+let playerWins = 0
+let playerLosses = 0
+let draws = 0
+
+function updateScores() {
+  document.getElementById('wins').innerHTML = playerWins
+  document.getElementById('losses').innerHTML = playerLosses
+  document.getElementById('draws').innerHTML = draws
+}
 
 function play() {
-  console.log('Play button pressed')
+  const randomNumber = Math.floor(Math.random() * 3)
+  const computersValue = 'RPS'.charAt(randomNumber)
+  const playersValue = prompt('Enter R, P or S')
+  if (playersValue === 'R') {
+    if (computersValue === 'R') {
+      draws++
+    }
+    if (computersValue === 'P') {
+      playerLosses++
+    }
+    if (computersValue === 'S') {
+      playerWins++
+    }
+  }
+  if (playersValue === 'P') {
+    if (computersValue === 'P') {
+      draws++
+    }
+    if (computersValue === 'S') {
+      playerLosses++
+    }
+    if (computersValue === 'R') {
+      playerWins++
+    }
+  }
+  if (playersValue === 'S') {
+    if (computersValue === 'S') {
+      draws++
+    }
+    if (computersValue === 'R') {
+      playerLosses++
+    }
+    if (computersValue === 'P') {
+      playerWins++
+    }
+  }
+  updateScores()
+  alert('Computer chose ' + computersValue)
 }
